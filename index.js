acb5fd27de7997c5f26358309956598156a662d7
daniel70125 <dwrighttt504@gmail.com>
2022-12-12 17:53:52 -0600

    Adding pictures for each character 20% done

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 
-const fullName = document.querySelector('#main-container-full-name');
 const imgDiv = document.querySelector('#img');
+const fullName = document.querySelector('#main-container-full-name');
+const birthYear = document.querySelector('#main-container-birth-year');
+const gender = document.querySelector('#main-container-gender');
+
 const loader = document.querySelector('#loader');
 const allCharactersMeter = document.querySelector('#allCharactersMeter');
+const aboutDetailsCont = document.querySelector('#aboutDetailsCont');
 
-let randomNumberPage = Math.floor(Math.random() * 11); // Random number between 0 - 10 for pages;
+let randomNumber = Math.floor(Math.random() * 82); // Random number between 0 - 81 for pages;
 
 const firstTenCharactersUrl = `https://swapi.dev/api/people/`; // URL to fetch all SWAPI characters
 let allCharactersList = [];
 let localImageCharactersList = [];
 let loadingAllCharacters = true;
+const lightSidePallette = ['#3780d3', '#5bdd59']
+const darkSidePallette = [ '#d62f00', '#950216']
 
 async function fetchLocalJson() {
-  await fetch('./swapi.json')
+  await fetch('./JSON/characters.json')
   .then((res) => res.json())
   .then((data) => {
     data.forEach((elm, index) => {
@@ -20,9 +26,8 @@ async function fetchLocalJson() {
     })
   })
   .catch((err) => console.log(err))
-  
 }
-fetchLocalJson()
+fetchLocalJson();
 // Add all characaters to array function
 addCharactersToArray = (arr) => {
   arr.forEach((elm, index) => {
@@ -35,9 +40,9 @@ checkMeter = (value) => {
 async function loadDoc() {
   await getAllCharacters();
   await addAllCharacterImages();
-  console.log(allCharactersList);
+  window.setTimeout(setData, 2000);
+  
 }
-
 addAllCharacterImages = () => {
   allCharactersList.forEach((elm, i) => {
     localImageCharactersList.forEach((element, index) => {
@@ -48,7 +53,13 @@ addAllCharacterImages = () => {
   })
 }
 setData = () => {
-  // fullName.innerText = 
+  let character = allCharactersList[randomNumber];
+  console.log(character)
+
+  fullName.innerText = `Name: ${character.name}`;
+  birthYear.innerText = `Birth Year: ${character.birth_year}`;
+  gender.innerText = `Gender: ${character.gender}`;
+  
 }
 // Loop over all pages and add to array; - MAIN FUNCTION
 async function getAllCharacters() {
@@ -78,6 +89,6 @@ async function getAllCharacters() {
     tl.to("#allCharactersMeter", { opacity:0 });
     tl.to("#loader", {  scale: 1.3 });
     tl.to("#loader", { scale: 0, opacity:0 });
+    tl.to("#aboutDetailsCont", {opacity:1});
 }
 document.onload = loadDoc();
-window.setTimeout(setData, 2000);
